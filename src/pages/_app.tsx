a07c057d7de689eeaf2d26763bf16eb2d7921361
Lucas Santana <lucas_377@hotmail.com>
2022-06-03 21:37:49 -0300

    Starting project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../ui/styles/globals.css'
+import type { AppProps } from 'next/app'
+import {ThemeProvider} from '@mui/material'
+import theme from '../ui/themes/default'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        Teste
+      </main>
+    </div>
+  )
+}
